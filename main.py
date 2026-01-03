53f21911f2276c5be88c57d84de47d7f42862150
Hussaini Lab <34137235+HussainiLab@users.noreply.github.com>
2026-01-02 23:46:56 -0500

    Fix main.py bug related to OMP initialization

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import sys
+import os
+
+# Workaround for OMP: Error #15: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized.
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Ensure the hfoGUI package is importable when launched from the repo root
 ROOT = Path(__file__).resolve().parent
